c73efa35437fd8680d71d2f171536e8501fbefe8
guiaf04 <guilhermearaujo@alu.ufc.br>
2024-08-06 00:59:08 -0300

    Kit 0, T0

---
+++ src/K0_T0/even_fibbonaci_numbers.java
@@ -0,0 +1,4 @@
+package K0_T0;
+
+public class even_fibbonaci_numbers {
+}
---
+++ src/K0_T0/multiple3_or_5.java
@@ -0,0 +1,7 @@
+package K0_T0;
+
+public class multiple3_of_5 {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
